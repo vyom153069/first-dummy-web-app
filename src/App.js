7c60fbec70406df12873d43e1498fc9a2009d895
vyom chandra gallani <vyomchandra@gmail.com>
2020-05-16 00:46:36 +0530

    footer done

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import  './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/layouts/Navbar';
+import Footer from './components/layouts/Footer';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <Navbar/>
+        <Footer/>
       </div>
     )
   }
---
+++ src/components/layouts/Footer.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import styled from 'styled-components';
+import '../../App.css';
+ function Footer() {
+    return (
+        <div className="main-footer fixed-bottom">
+        <FooterContainer >
+           <div className="container">
+               <div className="row">
+                {/* Column 1 */}
+                <div className="col-md-3 col-sm-6">
+                    <h4>Lorem Ispsum</h4>
+                    <ul className="list-unstyled">
+                        <li>loren ipsum</li>
+                        <li>loren ipsum</li>
+                        <li>loren ipsum</li>
+                        <li>loren ipsum</li>
+                    </ul>
+                </div>
+                {/* Column 2 */}
+                <div className="col-md-3 col-sm-6">
+                    <h4>Lorem Ispsum</h4>
+                    <ul className="list-unstyled">
+                        <li><a href="/">loren ipsum</a></li>
+                        <li><a href="/">loren ipsum</a></li>
+                        <li><a href="/">loren ipsum</a></li>
+                        <li><a href="/">loren ipsum</a></li>
+                    </ul>
+                </div>
+                {/* Column 3 */}
+                <div className="col-md-3 col-sm-6">
+                    <h4>Lorem Ispsum</h4>
+                    <ul className="list-unstyled">
+                        <li><a href="/">loren ipsum</a></li>
+                        <li><a href="/">loren ipsum</a></li>
+                        <li><a href="/">loren ipsum</a></li>
+                        <li><a href="/">loren ipsum</a></li>
+                    </ul>
+                </div>
+                {/* Column 4 */}
+                <div className="col-md-3 col-sm-6">
+                    <h4>Lorem Ispsum</h4>
+                    <ul className="list-unstyled">
+                        <li><a href="/">loren ipsum</a></li>
+                        <li><a href="/">loren ipsum</a></li>
+                        <li><a href="/">loren ipsum</a></li>
+                        <li><a href="/">loren ipsum</a></li>
+                    </ul>
+                </div>
+               </div>
+               {/* Footer Bottom */}
+                <div className="footer-bottom">
+    <p className="text-xs-center">&copy;{new Date().getFullYear()}City Guide App-All Rights are Resereved</p>
+                </div>
+
+           </div>
+           </FooterContainer>
+        </div>
+    )
+}
+export default Footer;
+
+
+const FooterContainer=styled.footer`
+
+    background: var(--mainDark);
+    color: var(--mainWhite);
+
+    ul li a{
+        color:var(--mainGray);
+    }
+    ul li a:hover{
+        color:var(--mainLightGray);
+    }
+`
