5ecc18bf21b4c414b33c7c8f15ab208047f50867
j-devil99 <jaidev.bhui@gmail.com>
2020-05-19 12:32:35 +0530

    fixed contact us page, and added navbar icons

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -6,29 +6,28 @@ class Contact extends Component {
     return (
       <section className="my-5 py-5">
         <div className="container">
-          <div className="well well-sm">
-            <h3>
-              <strong>Our Location</strong>
-            </h3>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-7">
-            <iframe
-              src="https://maps.google.com/maps?q=manhatan&t=&z=13&ie=UTF8&iwloc=&output=embed"
-              style={{
-                border: "0",
-                width: "100%",
-                height: "315px",
-                frameborder: "0",
-              }}
-            >
-              allowFullScreen
-            </iframe>
+          <div className="row">
+            <div className="col-md-7">
+              <h3>
+                <strong>Our Location</strong>
+              </h3>
+
+              <iframe
+                src="https://maps.google.com/maps?q=manhatan&t=&z=13&ie=UTF8&iwloc=&output=embed"
+                style={{
+                  border: "0",
+                  width: "100%",
+                  height: "315px",
+                  frameborder: "0",
+                }}
+              >
+                allowFullScreen
+              </iframe>
+            </div>
             <div className="col md-5">
-              <h4>
+              <h3>
                 <strong>Contact us</strong>
-              </h4>
+              </h3>
               <form>
                 <div className="form-group">
                   <input
@@ -48,7 +47,7 @@ class Contact extends Component {
                   <input
                     type="tel"
                     className="form-control"
-                    placeholder="phone"
+                    placeholder="Phone"
                   ></input>
                 </div>
                 <textarea
@@ -57,7 +56,7 @@ class Contact extends Component {
                   rows="3"
                   placeholder="Message"
                 />
-                <Link className="btn btn-outline-primary text-uppercase">
+                <Link className="btn btn-outline-primary text-uppercase w-100 mt-3">
                   <i className="fa-paper-plane-o" aria-hidden="true" />
                   <i className="fab fa-telegram-plane" />
                   &nbsp;send
